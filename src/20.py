eef7af8edda7e037b138138c3f761a70422d6194
Noah Hansson <hansson.noah@gmail.com>
2024-12-20 12:18:45 +0100

    Day 20

---
+++ src/20.py
@@ -0,0 +1,132 @@
+from utils import read_input, timer, setup_args, iter_input
+from classes import Point
+from collections import defaultdict
+from collections.abc import Iterator
+from functools import cache
+
+args = setup_args()
+
+
+def parse_input(test: bool = False) -> tuple[set[Point], Point, Point]:
+    inpt = read_input("20", test=test)
+
+    walls: set[Point] = set()
+    start: Point | None = None
+    end: Point | None = None
+
+    for x, y, c in iter_input(inpt):
+        if c == "#":
+            walls.add(Point(x, y))
+        if c == "S":
+            start = Point(x, y)
+        if c == "E":
+            end = Point(x, y)
+
+    assert start is not None
+    assert end is not None
+
+    return walls, start, end
+
+
+def get_neighbours(p: Point) -> Iterator[Point]:
+    directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+    for d in directions:
+        yield p + d
+
+
+def find_distances(
+    start: Point, end: Point, walls: set[Point]
+) -> dict[Point, int]:
+    distances: dict[Point, int] = {}
+    position = end
+    i = 0
+    while position != start:
+        distances[position] = i
+        neighbours = [
+            n
+            for n in get_neighbours(position)
+            if (n not in distances) and (n not in walls)
+        ]
+
+        if len(neighbours) == 0:
+            raise RuntimeError
+
+        assert len(neighbours) == 1
+
+        position = neighbours[0]
+        i += 1
+
+    distances[start] = i
+
+    return distances
+
+
+@cache
+def find_shortcuts(pos: Point, max_steps: int) -> set[Point]:
+    adj: set[Point] = {pos}
+
+    if max_steps == 0:
+        return adj
+
+    for neighbour in get_neighbours(pos):
+        adj |= find_shortcuts(neighbour, max_steps - 1)
+
+    return adj
+
+
+def solve(
+    cheat_min: int, cheat_max: int, threshold: int, test: bool = False
+) -> dict[int, int]:
+    walls, start, end = parse_input(test)
+
+    distances = find_distances(start, end, walls)
+
+    cheats: dict[int, int] = defaultdict(int)
+
+    for pos in distances.keys():
+        targets = {t for t in find_shortcuts(pos, cheat_max) if t in distances}
+        for target in targets:
+            if distances[pos] - distances[target] >= threshold:
+                cheat_length = (target - pos).abs()
+                if cheat_min <= cheat_length <= cheat_max:
+                    time_gain = (
+                        distances[pos] - distances[target] - cheat_length
+                    )
+
+                    cheats[time_gain] += 1
+
+    return cheats
+
+
+@timer
+def get_first_solution(test: bool = False):
+    if test:
+        threshold = 1
+    else:
+        threshold = 100
+    cheats = solve(cheat_min=2, cheat_max=2, threshold=threshold, test=test)
+    score = 0
+    for time_gain, n_cheats in cheats.items():
+        if time_gain >= threshold:
+            score += n_cheats
+
+    return score
+
+
+@timer
+def get_second_solution(test: bool = False):
+    if test:
+        threshold = 50
+    else:
+        threshold = 100
+    cheats = solve(cheat_min=2, cheat_max=20, threshold=threshold, test=test)
+    score = 0
+    for time_gain, n_cheats in cheats.items():
+        if time_gain >= threshold:
+            score += n_cheats
+
+    return score
+
+
+print(f"P1: {get_first_solution(test=args.test)}")
+print(f"P2: {get_second_solution(test=args.test)}")
--- src/classes.py
+++ src/classes.py
@@ -7,8 +7,33 @@ class Point:
     x: int
     y: int
 
-    def __add__(self, other: tuple[int, int]) -> Self:
-        return self.__class__(self.x + other[0], self.y + other[1])
+    def __add__(self, other: object) -> Self:
+        if isinstance(other, tuple):
+            if len(other) != 2:
+                raise ValueError(
+                    f"Tuple must be of length 2, recieved {len(other)}"
+                )
+            return self.__class__(self.x + other[0], self.y + other[1])
+        elif isinstance(other, self.__class__):
+            return self.__class__(self.x + other.x, self.y + other.y)
+        else:
+            raise ValueError(
+                f"Unsupported object for addition: {type(other)}. Supported classes: tuple, Point"
+            )
+
+    def __sub__(self, other: object) -> Self:
+        if isinstance(other, tuple):
+            if len(other) != 2:
+                raise ValueError(
+                    f"Tuple must be of length 2, recieved {len(other)}"
+                )
+            return self.__class__(self.x - other[0], self.y - other[1])
+        elif isinstance(other, self.__class__):
+            return self.__class__(self.x - other.x, self.y - other.y)
+        else:
+            raise ValueError(
+                f"Unsupported object for addition: {type(other)}. Supported classes: tuple, Point"
+            )
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, tuple):
@@ -27,3 +52,6 @@ class Point:
 
     def __le__(self, other: Self) -> bool:
         return self.x <= other.x
+
+    def abs(self) -> int:
+        return abs(self.x) + abs(self.y)
