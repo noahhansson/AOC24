577b92e100ee0897e153606b0e75cf3de1fe4870
Noah Hansson <hansson.noah@gmail.com>
2024-12-28 15:35:43 +0100

    Day 21 p2

--- src/21.py
+++ src/21.py
@@ -1,7 +1,6 @@
 from utils import read_input, timer, setup_args
 from functools import cache
 from collections import deque
-from itertools import product
 
 args = setup_args()
 
@@ -42,83 +41,62 @@ def get_neighbours_keypad(key: str) -> dict[str, str]:
 
 
 @cache
-def get_path(start: str, end: str, numpad: bool) -> list[list[str]]:
-    queue: deque[tuple[str, list[str]]] = deque()
+def get_path(key_from: str, key_to: str, numpad: bool = False) -> str:
+    if numpad:
+        neighbour_f = get_neighbours_numpad
+    else:
+        neighbour_f = get_neighbours_keypad
 
-    max_len: int | None = None
-    paths: list[list[str]] = []
-
-    queue.append((start, []))
+    queue: deque[tuple[str, str]] = deque()
+    queue.append(("", key_from))
 
     while queue:
-        position, path = queue.popleft()
+        path, curr_position = queue.popleft()
 
-        if max_len is not None and len(path) > max_len:
-            return paths
+        if curr_position == key_to:
+            return path
 
-        if position == end:
-            paths.append(path)
-            max_len = len(path)
+        # Add moves to queue in order of priority: "<v^>" due to distance from "A"-key
+        for direction, position in sorted(
+            neighbour_f(curr_position).items(), key=lambda x: "<v^>".find(x[0])
+        ):
+            if (direction not in path) or (direction == path[-1]):
+                queue.append((path + direction, position))
 
-        if numpad:
-            neighbours = get_neighbours_numpad(position)
-        else:
-            neighbours = get_neighbours_keypad(position)
-
-        for direction, button in neighbours.items():
-            queue.append((button, path + [direction]))
-
-    return []
+    return ""
 
 
-def press_instructions(instructions: str, numpad: bool) -> list[str]:
-    path_alternatives = []
-    for b1, b2 in zip(f"A{instructions}", instructions):
-        path_alternatives.append(get_path(b1, b2, numpad=numpad))
+@cache
+def count_presses(
+    key_from: str, key_to: str, n_robots: int, numpad: bool
+) -> int:
+    path = get_path(key_from, key_to, numpad)
+    keys = f"A{path}A"
 
-    paths = product(*path_alternatives)
-    paths_formatted: list[str] = []
-    for path in paths:
-        path_formatted = []
-        for instruction in path:
-            path_formatted += instruction
-            path_formatted += "A"
-        paths_formatted.append("".join(path_formatted))
+    if n_robots == 0:
+        return len(f"{path}A")
+    else:
+        score = 0
+        for k_from, k_to in zip(keys, keys[1:]):
+            score += count_presses(k_from, k_to, n_robots - 1, False)
 
-    return paths_formatted
-
-
-def prune_instructions(instructions: list[str]) -> list[str]:
-    lenghts = {i: len(i) for i in instructions}
-    min_len = min(lenghts.values())
-    return [i for i, length in lenghts.items() if length == min_len]
+        return score
 
 
 def solve(n_robots: int, test: bool) -> int:
     codes = parse_input(test)
-    get_path("A", "<", False)
 
-    score = 0
+    total = 0
 
     for code in codes:
-        instructions = press_instructions(code, numpad=True)
+        score = 0
+        keys = f"A{code}"
+        for key_from, key_to in zip(keys, keys[1:]):
+            score += count_presses(key_from, key_to, n_robots, True)
 
-        for i in range(n_robots):
-            print(f"Code: {code}, Robot nr {i + 1}")
-            print(f"Number of instructions to parse: {len(instructions)}")
-            instructions_next: list[str] = []
-            for instruction in prune_instructions(instructions):
-                instructions_next += press_instructions(
-                    instruction, numpad=False
-                )
+        total += int(code.replace("A", "")) * score
 
-            instructions = instructions_next
-
-        min_len = len(prune_instructions(instructions_next)[0])
-        score += int(code.replace("A", "")) * min_len
-        print(f"Code: {code}, min length: {min_len}")
-
-    return score
+    return total
 
 
 @timer
